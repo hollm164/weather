3c58bd1fcff7d35776ee8e14d1065734626bd849
hollm164 <marielleholland@marielles-mbp.wireless.newschool.edu>
2016-04-26 18:15:18 -0400

    still working!

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 var cities;
 var city;
+var today;
+
 
 function setup() {
 
   cities = createSelect();
   cities.option('New York,NY');
   cities.option('Miami, FL');
+  cities.option('Los Angeles, CA');
   //Change these cities and/or add your own
 
   cities.changed(loadCity);
@@ -16,14 +19,70 @@ function setup() {
 }
 
 function draw() {
+
+
+
   if(city){ //Your drawing should go inside this if statement
 
-    background("grey");
-    text(city.name,50,50); //feel free to delete this
+//change background based on temperature
+    if (city.main.temp <30){
+      background("#F2F2F2");
+    }else if (city.main.temp>30 && city.main.temp<45){
+      background("#AED6F1");
+    }else if (city.main.temp>45 && city.main.temp<65){
+      background ("#EDBB99");
+    }else if (city.main.temp>65 && city.main.temp<100){
+      background("#F1948A");
+    }
 
+//city name
+    textSize(30);
+    fill("#6E6E6E");
+    text(city.name,10,40);
+    textSize(15);
 
+//day, time, and weather
+    var today = new Date();
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0]= "Sunday";
+    weekday[1]= "Monday";
+    weekday[2]= "Tuesday";
+    weekday[3]= "Wednesday";
+    weekday[4]= "Thursday";
+    weekday[5]= "Friday";
+    weekday[6]= "Saturday";
+
+    var n = weekday[d.getDay()];
+    var h = today.getHours();
+    var mm = today.getMinutes();
+
+    today = n+' '+h+':'+mm;
+
+    textSize(20);
+    text(today,10,65);
+    text(city.weather[0].main, 10,85);
+
+//temperature
+    textSize(80);
+    fill('black');
+    text(int(city.main.temp), 155,180);
+    textSize(15);
+    text('°F',245,135);
+
+//sunrise
+  if(city.sunrise||city.sunset){
+    noStroke();
+    fill("#F7DC6F");
+    ellipse(200,400,100,100);
+  }else{
+    noStroke();
+    fill("#F7DC6F");
+    ellipse(200,280,100,100);
+    }
   }
 }
+
 
 //These functions below load the weather data and save it to the city variable.
 //There is no need to edit these functions.
