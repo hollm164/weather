6c80dc0ad6159fa51fb3d7032174bd3872685f0a
hollm164 <hollm164@newschool.edu>
2016-04-30 16:18:58 -0400

    done!
    This weather app changes the background color based on temperature. The
    sun position sets and rises during those times and the sun also melts
    if the humidity goes above 40%. Main weather and wind speed are also
    displayed.

--- sketch.js
+++ sketch.js
@@ -59,9 +59,11 @@ function draw() {
 
     today = n+' '+h+':'+mm;
 
+//main weather
     textSize(20);
     text(today,10,65);
     text(city.weather[0].main, 10,85);
+
 
 //temperature
     textSize(80);
@@ -80,7 +82,37 @@ function draw() {
     fill("#F7DC6F");
     ellipse(200,280,100,100);
     }
+
+
+//humidity
+  textSize(15);
+  fill("#6E6E6E");
+  text("Humidity:",10,390);
+  text(city.main.humidity,80,390);
+  text("%",100,390);
+
+  if(city.main.humidity>40){
+    fill("#F7DC6F");
+    ellipse(200,330,10,50);
+    ellipse(210,330,15,20);
+    ellipse(190,330,13,80);
+    ellipse(180,325,20,20);
+    ellipse(217,330,10,100);
+    ellipse(225,330,10,40);
+    ellipse(235,310,15,20);
   }
+
+//wind speed
+  textSize(15);
+  fill("#6E6E6E");
+  text("Wind:",290,390);
+  text(int(city.wind.speed),340,390);
+  text("mph",360,390);
+
+
+
+  }
+
 }
 
 
